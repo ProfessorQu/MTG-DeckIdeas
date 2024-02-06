1658100cb9d773ee0b92344cd0acf0f99d04b4ce
ProfessorQu <quint@dasookwat.com>
2024-02-06 19:58:17 +0100

    Figured out how to use functions from other files (just put them in HTML)

--- scripts/deck_restrictions.js
+++ scripts/deck_restrictions.js
@@ -1,38 +1,3 @@
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-
-    while (currentIndex > 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
-}
-
-function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const sleep = ms => new Promise(r => setTimeout(r, ms));
-
-function createList(array) {
-    let list = document.getElementById("options");
-
-    for (let restriction of shuffle(array)) {
-        let listItem = document.createElement('li');
-        listItem.classList.add("list-group-item");
-        listItem.innerText = restriction;
-        listItem.style.top = "0px";
-
-        list.appendChild(listItem);
-    }
-}
-
 async function chooseRandom() {
     if (spinning) {
         return
@@ -43,7 +8,7 @@ async function chooseRandom() {
     let list = document.getElementById("options");
     list.innerHTML = "";
 
-    createList(RESTRICTIONS);
+    createList("options", RESTRICTIONS);
 
     list.style.maxHeight = "5em";
     let items = list.getElementsByClassName("list-group-item");
@@ -123,7 +88,7 @@ const RESTRICTIONS = [
     "All cards must be snow cards",
 ];
 
-createList(RESTRICTIONS);
+createList("options", RESTRICTIONS);
 
 let selectRandomButton = document.getElementById("selectRandom");
 selectRandomButton.addEventListener('click', chooseRandom);
--- scripts/wincons.js
+++ scripts/wincons.js
@@ -1,38 +1,3 @@
-function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-
-  while (currentIndex > 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
-
-function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const sleep = ms => new Promise(r => setTimeout(r, ms));
-
-function createList(id, array) {
-    let list = document.getElementById(id);
-
-    for (let restriction of shuffle(array)) {
-        let listItem = document.createElement('li');
-        listItem.classList.add("list-group-item");
-        listItem.innerText = restriction;
-        listItem.style.top = "0px";
-
-        list.appendChild(listItem);
-    }
-}
-
 async function chooseRandom() {
     if (spinning) {
         return
